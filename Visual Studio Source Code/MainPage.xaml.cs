990e9870ecba98fdae32a07860aa918bbe293427
HookOwen <owmiho@gmail.com>
2024-10-23 17:45:33 -0400

    Update MainPage.xaml.cs
    Fixed it so it should populate the values

--- Visual Studio Source Code/MainPage.xaml.cs
+++ Visual Studio Source Code/MainPage.xaml.cs
@@ -11,22 +11,22 @@ namespace WellnessApp
         {
             InitializeComponent();
             _themeRepo = dbService;
+
+            //statusMessage.Text = "";
+            if (_themeRepo.GetThemes() == null) // Try setting this condtion to 'true' if the second time running the program and clicking the button doesn't show any results. make sure to switch it back afterwards though.
+            {
+                //Application.Current?.CloseWindow(Application.Current.MainPage.Window);
+
+                _themeRepo.Create(new Theme { Name = "Studying", Description = "Sample Text" });
+                _themeRepo.Create(new Theme { Name = "Fitness", Description = "Sample Text" });
+                _themeRepo.Create(new Theme { Name = "Language", Description = "Sample Text" });
+                _themeRepo.Create(new Theme { Name = "Instrument", Description = "Sample Text" });
+                _themeRepo.Create(new Theme { Name = "Mental Wellness", Description = "Sample Text" });
+            }
         }
 
         public async void ShowThemesClicked(object sender, EventArgs e)
         {
-
-            //statusMessage.Text = "";
-            //if (_themeRepo.GetThemes() == null)
-            //{
-            //    Application.Current?.CloseWindow(Application.Current.MainPage.Window);
-
-            //    await _themeRepo.Create(new Theme { Name = "Studying", Description = "Sample Text" });
-            //    await _themeRepo.Create(new Theme { Name = "Fitness", Description = "Sample Text" });
-            //    await _themeRepo.Create(new Theme { Name = "Language", Description = "Sample Text" });
-            //    await _themeRepo.Create(new Theme { Name = "Instrument", Description = "Sample Text" });
-            //    await _themeRepo.Create(new Theme { Name = "Mental Wellness", Description = "Sample Text" });
-            //}
 
             List<Theme> themes = await _themeRepo.GetThemes();
             ThemeList.ItemsSource = themes;
