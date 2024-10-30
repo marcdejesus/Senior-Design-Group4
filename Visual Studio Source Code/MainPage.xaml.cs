3856c8a3e5de509847617df3e5bbcbc046b52c05
Marc De Jesus <marcantoniodejesus@yahoo.com>
2024-10-30 11:12:37 -0400

    Page Functionality
    can now swap between pages, place holders for individual page functions for now

--- Visual Studio Source Code/MainPage.xaml.cs
+++ Visual Studio Source Code/MainPage.xaml.cs
@@ -35,6 +35,31 @@ namespace WellnessApp
 
         }
 
+        // Navigation button click handlers
+        private async void OnHomeButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//MainPage"); // Navigates to Home
+        }
+
+        private async void OnSearchButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SearchPage");
+        }
+
+        private async void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//AddPage");
+        }
+
+        private async void OnActivityButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ActivityPage");
+        }
+
+        private async void OnProfileButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ProfilePage");
+        }
     }
 
 }
