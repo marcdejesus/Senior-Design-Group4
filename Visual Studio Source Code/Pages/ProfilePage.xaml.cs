30b8812b6c82e2bdf55883e462f07e9256a2f9f0
Marc De Jesus <marcantoniodejesus@yahoo.com>
2024-11-23 20:20:54 -0500

    Revert "Darkmode"
    This reverts commit c3a49cdf2f6987cad1af0c0e88d190422829a6f0.

--- Visual Studio Source Code/Pages/ProfilePage.xaml.cs
+++ Visual Studio Source Code/Pages/ProfilePage.xaml.cs
@@ -1,37 +1,35 @@
-﻿using System;
+﻿//using WellnessApp.Database.Items;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+//using WellnessApp.Database.Repository;
+using WellnessApp.Database.Services;
+using WellnessApp.Items;
 
 namespace WellnessApp
 {
     public partial class ProfilePage : ContentPage
     {
+
         public ProfilePage()
         {
             InitializeComponent();
-            SetTheme(Application.Current.UserAppTheme);
-        }
-
-         private void SetTheme(AppTheme theme)
-        {
-            if (theme == AppTheme.Dark)
-            {
-                this.BackgroundColor = Colors.DarkGray;
-            }
-            else
-            {
-                this.BackgroundColor = Colors.White;
-            }
         }
 
         // Navigation button click handlers
         private async void OnHomeButtonClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//MainPage");
+            await Shell.Current.GoToAsync("//MainPage"); // Navigates to Home
         }
 
         private async void OnCalendarButtonClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//CalendarPage");
+        }
+
+        private async void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//AddPage");
         }
 
         private async void OnExploreButtonClicked(object sender, EventArgs e)
@@ -44,9 +42,6 @@ namespace WellnessApp
             await Shell.Current.GoToAsync("//ProfilePage");
         }
 
-        private async void OnSettingsButtonClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//SettingsPage");
-        }
     }
+
 }
